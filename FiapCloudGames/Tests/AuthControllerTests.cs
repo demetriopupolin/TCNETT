3e24395b4af0f23b4126eb535675a70ca2c0b66a
demetriopupolin <pupolin@gmail.com>
2025-07-30 05:03:33 -0700

    teste
    teste

--- FiapCloudGames/Tests/AuthControllerTests.cs
+++ FiapCloudGames/Tests/AuthControllerTests.cs
@@ -19,8 +19,125 @@ namespace Tests
             _output = output;
         }
 
+
         [Fact]
-        public void Login_DeveRetornarToken_QuandoCredenciaisForemValidas()
+        public void CTAC001_Autenticacao_de_usuario_nao_sucedida()
+        {
+            // Arrange
+            var email = "teste@email.com";
+            var senha = "123456@X";
+            var nome = "Teste";
+            var mockRepo = new Mock<IUsuarioRepository>();
+            mockRepo.Setup(r => r.ObterPorEmail(email)).Returns(new Usuario
+            {
+                Id = 1,
+                Nome = nome,
+                Email = email,
+                Senha = senha,
+                Nivel = 'U'
+            });
+
+            var mockConfig = new Mock<IConfiguration>();
+            mockConfig.Setup(c => c["Jwt:Key"]).Returns("minha-chave-super-secretaaaaaaaaaaaaaaaaaaaaaaaaaa-1234567890abcd");
+            mockConfig.Setup(c => c["Jwt:Issuer"]).Returns("FiapIssuer");
+
+            var controller = new AuthController(mockConfig.Object, mockRepo.Object);
+
+            var loginDto = new LoginDto
+            {
+                Email = email,
+                Senha = senha
+            };
+
+            // Act
+            var resultado = controller.Login(loginDto);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(resultado);
+            Assert.NotNull(okResult.Value);
+            _output.WriteLine($"Token gerado: {okResult.Value}");
+        }
+
+        [Fact]
+        public void CTAC002_Autenticacao_de_usuario_bem_sucedida()
+        {
+            // Arrange
+            var email = "teste@email.com";
+            var senha = "123456@A";
+            var nome = "Teste";
+            var mockRepo = new Mock<IUsuarioRepository>();
+            mockRepo.Setup(r => r.ObterPorEmail(email)).Returns(new Usuario
+            {
+                Id = 1,
+                Nome = nome,
+                Email = email,
+                Senha = senha,
+                Nivel = 'U'
+            });
+
+            var mockConfig = new Mock<IConfiguration>();
+            mockConfig.Setup(c => c["Jwt:Key"]).Returns("minha-chave-super-secretaaaaaaaaaaaaaaaaaaaaaaaaaa-1234567890abcd");
+            mockConfig.Setup(c => c["Jwt:Issuer"]).Returns("FiapIssuer");
+
+            var controller = new AuthController(mockConfig.Object, mockRepo.Object);
+
+            var loginDto = new LoginDto
+            {
+                Email = email,
+                Senha = senha
+            };
+
+            // Act
+            var resultado = controller.Login(loginDto);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(resultado);
+            Assert.NotNull(okResult.Value);
+            _output.WriteLine($"Token gerado: {okResult.Value}");
+        }
+
+
+
+        [Fact]
+        public void CTAC003_Autenticacao_de_usuario_administrador_nao_sucedida()
+        {
+            // Arrange
+            var email = "teste@email.com";
+            var senha = "123456@X";
+            var nome = "Teste";
+            var mockRepo = new Mock<IUsuarioRepository>();
+            mockRepo.Setup(r => r.ObterPorEmail(email)).Returns(new Usuario
+            {
+                Id = 1,
+                Nome = nome,
+                Email = email,
+                Senha = senha,
+                Nivel = 'A'
+            });
+
+            var mockConfig = new Mock<IConfiguration>();
+            mockConfig.Setup(c => c["Jwt:Key"]).Returns("minha-chave-super-secretaaaaaaaaaaaaaaaaaaaaaaaaaa-1234567890abcd");
+            mockConfig.Setup(c => c["Jwt:Issuer"]).Returns("FiapIssuer");
+
+            var controller = new AuthController(mockConfig.Object, mockRepo.Object);
+
+            var loginDto = new LoginDto
+            {
+                Email = email,
+                Senha = senha
+            };
+
+            // Act
+            var resultado = controller.Login(loginDto);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(resultado);
+            Assert.NotNull(okResult.Value);
+            _output.WriteLine($"Token gerado: {okResult.Value}");
+        }
+
+        [Fact]
+        public void CTAC004_Autenticacao_de_usuario_administrador_bem_sucedida()
         {
             // Arrange
             var email = "teste@email.com";
@@ -57,72 +174,8 @@ namespace Tests
             _output.WriteLine($"Token gerado: {okResult.Value}");
         }
 
-        [Fact]
-        public void Login_DeveRetornarUnauthorized_QuandoSenhaForInvalida()
-        {
-            // Arrange
-            var email = "teste@email.com";
-            var senhaCorreta = "123456@A";
-            var senhaIncorreta = "senhaErrada";
-            var mockRepo = new Mock<IUsuarioRepository>();
-            mockRepo.Setup(r => r.ObterPorEmail(email)).Returns(new Usuario
-            {
-                Id = 1,
-                Nome = "Teste",
-                Email = email,
-                Senha = senhaCorreta,
-                Nivel = 'A'
-            });
 
-            var mockConfig = new Mock<IConfiguration>();
-            mockConfig.Setup(c => c["Jwt:Key"]).Returns("minha-chave-super-secretaaaaaaaaaaaaaaaaaaaaaaaaaa-1234567890abcd");
-            mockConfig.Setup(c => c["Jwt:Issuer"]).Returns("FiapIssuer");
 
-            var controller = new AuthController(mockConfig.Object, mockRepo.Object);
 
-            var loginDto = new LoginDto
-            {
-                Email = email,
-                Senha = senhaIncorreta
-            };
-
-            // Act
-            var resultado = controller.Login(loginDto);
-
-            // Assert
-            var unauthorizedResult = Assert.IsType<UnauthorizedObjectResult>(resultado);
-            Assert.Equal("Senha incorreta.", unauthorizedResult.Value);
-            _output.WriteLine($"Mensagem retornada: {unauthorizedResult.Value}");
-        }
-
-        [Fact]
-        public void Login_DeveRetornarUnauthorized_QuandoUsuarioNaoExistir()
-        {
-            // Arrange
-            var email = "inexistente@email.com";
-            var senha = "qualquerSenha";
-            var mockRepo = new Mock<IUsuarioRepository>();
-            mockRepo.Setup(r => r.ObterPorEmail(email)).Returns((Usuario)null);
-
-            var mockConfig = new Mock<IConfiguration>();
-            mockConfig.Setup(c => c["Jwt:Key"]).Returns("minha-chave-super-secretaaaaaaaaaaaaaaaaaaaaaaaaaa-1234567890abcd");
-            mockConfig.Setup(c => c["Jwt:Issuer"]).Returns("FiapIssuer");
-
-            var controller = new AuthController(mockConfig.Object, mockRepo.Object);
-
-            var loginDto = new LoginDto
-            {
-                Email = email,
-                Senha = senha
-            };
-
-            // Act
-            var resultado = controller.Login(loginDto);
-
-            // Assert
-            var unauthorizedResult = Assert.IsType<UnauthorizedObjectResult>(resultado);
-            Assert.Equal($"Usuário {email} não encontrado.", unauthorizedResult.Value);
-            _output.WriteLine($"Mensagem retornada: {unauthorizedResult.Value}");
-        }
     }
 }
