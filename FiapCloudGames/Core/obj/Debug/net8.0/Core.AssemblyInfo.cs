16a16ea0304c1eff53b133792737234fb020373c
demetriopupolin <pupolin@gmail.com>
2025-08-04 05:11:28 -0700

    ajuste
    ajuste

--- FiapCloudGames/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ FiapCloudGames/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d483e567254024723c42fe6011ce6ced62814d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4df7b674663d4979066b004e816164400e8ceb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FiapCloudGames/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ FiapCloudGames/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d483e567254024723c42fe6011ce6ced62814d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4df7b674663d4979066b004e816164400e8ceb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
